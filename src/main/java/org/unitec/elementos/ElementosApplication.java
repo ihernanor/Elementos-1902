8bef696225b7c1fbe09dd5a0cb5787bd7c7e97f4
T-107 <T-107@PC191>
2019-01-25 21:33:54 -0600

    Proyecto Spring. Primer push

---
+++ src/main/java/org/unitec/elementos/ElementosApplication.java
@@ -0,0 +1,14 @@
+package org.unitec.elementos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElementosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElementosApplication.class, args);
+	}
+
+}
+
