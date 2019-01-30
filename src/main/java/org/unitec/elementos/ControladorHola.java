43ca6c961d8e16aea9880d46bba4abc108b6afc1
T-107 <T-107@PC191>
2019-01-29 21:27:17 -0600

    elemento

---
+++ src/main/java/org/unitec/elementos/ControladorHola.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unitec.elementos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+
+public class ControladorHola {
+ 
+    @GetMapping("/")
+    public String holaMundo(){
+        return "Mi primer servicio REST :D";
+    } 
+}
