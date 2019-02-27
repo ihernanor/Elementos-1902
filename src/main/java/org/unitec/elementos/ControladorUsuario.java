bdd797eb945ca1d884e07316aa51b60e64349740
T-107 <T-107@PC191>
2019-02-26 21:37:39 -0600

    Postman

--- src/main/java/org/unitec/elementos/ControladorUsuario.java
+++ src/main/java/org/unitec/elementos/ControladorUsuario.java
@@ -6,9 +6,14 @@
 package org.unitec.elementos;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +35,36 @@ public class ControladorUsuario {
         Usuario u = maper.readValue(json, Usuario.class);
         repoU.save(u);
         Estatus e=new Estatus("Usuario guardado", true);
+        return e;
+    }
+    
+    //2. Buscar todos
+    @GetMapping("/usuario")
+    List<Usuario> buscarTodos(){
+      return repoU.findAll();
+    }
+    
+    //3. Buscar por id
+    @GetMapping("/usuario/{id}")
+    Usuario buscarPorId(@PathVariable Integer id){
+        return repoU.findById(id).get();
+    }
+    
+    //4. Actualizar
+    @PutMapping("/usuario")
+    Estatus actualizar(@RequestBody String json)throws Exception{
+        ObjectMapper mapper= new ObjectMapper();
+        Usuario u=mapper.readValue(json, Usuario.class);
+        repoU.save(u);
+        Estatus e=new Estatus("Usuario actualizado", true);
+        return e;
+    }
+    
+    //5. Borrar por id
+    @DeleteMapping("/usuario/{id}")
+    Estatus borrar(@PathVariable Integer id){
+        repoU.deleteById(id);
+        Estatus e=new Estatus("Usuario borrado con éxito ", true);
         return e;
     }
 }
